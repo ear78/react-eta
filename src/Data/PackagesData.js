8df435328f6f6ac70902281e258346310d9ffe7c
elliot richardson <ear78@aol.com>
2018-02-19 00:21:13 -0600

    added packages data and created data json file.

---
+++ src/Data/PackagesData.js
@@ -0,0 +1,33 @@
+const packagesData = [
+    {
+        "city": "Vancouver",
+        "country": "Canada",
+        "price": 1499,
+        "days": 7,
+        "img": require("../Images/paul-morris-mtn.jpg")
+    },
+    {
+        "city": "Vancouver",
+        "country": "Canada",
+        "price": 1499,
+        "days": 7,
+        "img": require("../Images/paul-morris-mtn.jpg")
+    },
+    {
+        "city": "Vancouver",
+        "country": "Canada",
+        "price": 1499,
+        "days": 7,
+        "img": require("../Images/tim-trad-truck.jpg")
+    },
+    {
+        "city": "Vancouver",
+        "country": "Canada",
+        "price": 1499,
+        "days": 7,
+        "img": require("../Images/forrest-cavale-lights.jpg")
+    },
+
+];
+
+export default packagesData;
--- src/Pages/Packages/Packages.js
+++ src/Pages/Packages/Packages.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import packagesData from '../../Data/PackagesData';
 import './Packages.css';
 
 class Packages extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            trips: packagesData
+        }
+    }
     render(){
+        console.log(packagesData);
+        const packagesList = this.state.trips.map((trip,index) => {
+            return (
+                <div key={index}>
+                    <p><img src={trip.img} /></p>
+                    <p>Destination - {trip.country}, {trip.city}</p>
+                    <p>Days - {trip.days}</p>
+                    <p>Price - {trip.price.toLocaleString()}</p>
+                </div>
+            )
+
+        })
         return (
-            <section className="Packages">Packages</section>
+            <section className="Packages">{packagesList}</section>
+
         )
     }
 }
